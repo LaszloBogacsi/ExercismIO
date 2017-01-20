993ee10e35baf3f8e7ec358081a7f3be126d54e5
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-01-20 16:45:17 +0000

    regex validation completed

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -4,7 +4,24 @@ function Words () {
 }
 
 Words.prototype.count = function (words) {
+  var wordCounts = {};
+ words = words.toLowerCase()
+              .replace(/[.,\/#!¡¿$%\^&\*;@:{}=\-_`~()]/g,' ') // replace all non-alphanumeric and non-whitespace and non-apostrophe characters with whitespace.
+              .replace(/'\B/g, ' ') // ignoring single-quotation at the back of the word
+              .replace(/[\n\t?]/,' '); // replacing linefeed and tab charachter with whitespace
+ var wordsArray = words.split(' '); // put the word in an array splitted at whitespace
 
+ wordsArray.map(function(word){
+   word = word.trim(); // getting rid of white space around a word.
+   word = word.replace(/^'/,'') // ignore the single-quotation at the start of the word.
+   if (word in wordCounts && word !== 'constructor') {
+
+     wordCounts[`${word}`] += 1;
+   } else if (word !== '') {
+     wordCounts[`${word}`] = 1;
+   }
+ });
+ return wordCounts;
 };
 
 module.exports = Words;
