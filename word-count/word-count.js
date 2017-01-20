67b4b6de3148c38cf1d08a19fb959b80f73c2aca
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-01-20 17:13:18 +0000

    minor refator

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -6,19 +6,18 @@ function Words () {
 Words.prototype.count = function (words) {
   var wordCounts = {};
  words = words.toLowerCase()
-              .replace(/[.,\/#!¡¿$%\^&\*;@:{}=\-_`~()]/g,' ') // replace all non-alphanumeric and non-whitespace and non-apostrophe characters with whitespace.
+              .replace(/[.,\/#!¡¿$%\^&\*;@:{}=\-_`~()]/g,' ') // all the punctuation with whitespace
               .replace(/'\B/g, ' ') // ignoring single-quotation at the back of the word
-              .replace(/[\n\t?]/,' '); // replacing linefeed and tab charachter with whitespace
+              .replace(/[\n\t?]/,' ') // replacing linefeed and tab charachter with whitespace
+              .trim();
  var wordsArray = words.split(' '); // put the word in an array splitted at whitespace
-
+console.log(wordsArray);
  wordsArray.map(function(word){
-   word = word.trim(); // getting rid of white space around a word.
    word = word.replace(/^'/,'') // ignore the single-quotation at the start of the word.
-   if (word in wordCounts && word !== 'constructor') {
-
-     wordCounts[`${word}`] += 1;
+   if (word in wordCounts && wordCounts.hasOwnProperty(word) ) {
+     wordCounts[word] += 1;
    } else if (word !== '') {
-     wordCounts[`${word}`] = 1;
+     wordCounts[word] = 1;
    }
  });
  return wordCounts;
