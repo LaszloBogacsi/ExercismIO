5a293f1c51a218842019443f828116aa6fa4dbbd
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-01-28 23:04:25 +0000

    refactor

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -2,26 +2,31 @@ function Anagram(word) {
   this.word = word.toLowerCase();
 }
 
+
+var sortLetters = function(word) {
+  return word.split('').sort().join('')
+}
+
+var hasSameLetters = function(word1, word2){
+  return sortLetters(word1) === sortLetters(word2);
+}
+
+var isEqualLengthAndNotSame = function(word1, word2) {
+  return word1.length === word2.length && word1 !== word2;
+}
+
 Anagram.prototype.matches = function (wordsArr) {
-  var word = this.word;
-  var wordsArr = wordsArr;
   anagrams = [];
-  if (typeof arguments[0] == 'string') {
-    wordsArr = Array.from(arguments);
-  }
-  for (var i = 0; i < wordsArr.length; i++){
-    var possibleWord = wordsArr[i].toLowerCase();
-    if (wordsArr[i].length === word.length && possibleWord !== this.word){
-      for (var j = 0; j < word.length; j++){
-        if (possibleWord.includes(word[j])){
-          possibleWord = possibleWord.replace(word[j], '')
-          if (possibleWord === ''){
-            anagrams.push(wordsArr[i]);
-          }
-        }
-      }
+  var that = this
+  if (typeof arguments[0] !== 'object') { wordsArr = Array.from(arguments); }
+
+  wordsArr.map(function(match){
+    var possibleMatch = match.toLowerCase();
+    if (isEqualLengthAndNotSame(possibleMatch, that.word) && hasSameLetters(possibleMatch, that.word)){
+      anagrams.push(match);
     }
-  }
+  });
+
   return anagrams;
 };
 
