30ac7fe7f863d5c7dad81206f1f42ba488cb7588
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-02-03 15:40:08 +0000

    pangram done

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,3 +1,15 @@
-def is_pangram(sentence):
+import re
 
+def is_pangram(sentence):
+    stripped = stripSentence(sentence)
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    for i in range(len(alphabet)):
+        if not alphabet[i] in stripped:
+            return False
     return True
+
+def stripSentence(sentence):
+    sentence = sentence.lower()
+    result = re.findall(r"[a-zA-Z]", sentence)
+    return result
