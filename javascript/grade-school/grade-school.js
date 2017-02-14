6d4524d92338623dbc2451b8d5f73ae94b9b5518
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-02-14 12:06:30 +0000

    can add multiple students to same grade

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -8,8 +8,12 @@ return this.studentList
 };
 
 School.prototype.add = function (name, grade) {
+  if (!this.studentList[grade]) {
+    this.studentList[grade] = [name]
+  } else {
+    this.studentList[grade].push(name)
+  }
 
-this.studentList[grade] = [name]
 };
 
 module.exports = School;
