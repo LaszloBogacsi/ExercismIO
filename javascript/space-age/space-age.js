c0e5478f36aadf943072132792687a447c874571
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-02-16 15:57:25 +0000

    refactoring

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -14,55 +14,49 @@ var roundToTwoDecimals = function (number) {
 
 SpaceAge.prototype.calculateAge = function (planetaryMultiplier) {
   var age = this.seconds / (this.earthYearSeconds * planetaryMultiplier);
+  age = roundToTwoDecimals(age);
   return age;
 };
 
 SpaceAge.prototype.onEarth = function () {
   const multiplier = 1;
   var age = this.calculateAge(multiplier);
-  age = roundToTwoDecimals(age);
   return age;
 };
 
 SpaceAge.prototype.onMercury = function () {
   const multiplier = 0.2408467;
   var age = this.calculateAge(multiplier);
-  age = roundToTwoDecimals(age);
   return age;
 };
 
 SpaceAge.prototype.onVenus = function () {
   const multiplier = 0.61519726;
   var age = this.calculateAge(multiplier);
-  age = roundToTwoDecimals(age);
   return age;
 };
 
 SpaceAge.prototype.onMars = function () {
   const multiplier = 1.8808158;
   var age = this.calculateAge(multiplier);
-  age = roundToTwoDecimals(age);
   return age;
 };
 
 SpaceAge.prototype.onJupiter = function () {
   const multiplier = 11.862615;
   var age = this.calculateAge(multiplier);
-  age = roundToTwoDecimals(age);
   return age;
 };
 
 SpaceAge.prototype.onSaturn = function () {
   const multiplier = 29.447498;
   var age = this.calculateAge(multiplier);
-  age = roundToTwoDecimals(age);
   return age;
 };
 
 SpaceAge.prototype.onUranus = function () {
   const multiplier = 84.016846;
   var age = this.calculateAge(multiplier);
-  age = roundToTwoDecimals(age);
   return age;
 };
 
