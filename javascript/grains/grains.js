bd9ad38d9d4fad1d31bb9c4f0fb5625b145d7507
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-02-17 15:29:28 +0000

    can total all the squares

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -7,8 +7,17 @@ function Grains() {
 
 Grains.prototype.square = function (power) {
   var bigNum = bigInt(2).pow(power - 1);
-  console.log(bigNum);
+  // console.log(bigNum);
   return bigNum.toString();
+};
+
+Grains.prototype.total = function () {
+  var total = 0;
+  for (var i = 1; i < 65; i++) {
+    total = bigInt(total).add(this.square(i))
+  };
+
+  return total.toString();
 };
 
 
