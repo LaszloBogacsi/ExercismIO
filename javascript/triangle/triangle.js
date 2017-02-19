2d0b4728c129de9a0b91e49a39ca309d282140f2
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-02-19 13:54:58 +0000

    further refactoring using arrow functions

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -2,38 +2,34 @@ function Triangle(x, y, z) {
 this.sides = [x, y, z];
 }
 
-
-var invalid = function(sides) {
-  var hasNoSize = (sides.reduce((sum, side)=>{ return sum + side}, 0) === 0);
-  if (hasNoSize) {return true};
-
-  var anyTwoSidesLessThanThird = (sides[0] + sides[1] < sides[2] || sides[0] + sides[2] < sides[1] || sides[1] + sides[2] < sides[0] );
-  if (anyTwoSidesLessThanThird) {return true} else {return false};
-
-}
-
 var names = {
   1: 'scalene',
   2: 'isosceles',
-  3: 'equilateral'}
+  3: 'equilateral'};
 
-var numberOfEqualSides = function(sides){
-  var arrOfEqualSides = sides.map(function(oneSide){
-    return sides.filter(function(side){
-      return side === oneSide
-    }).length;
+var invalid = sides => {
+  var hasNoSize = (sides.reduce((sum, side)=> sum + side, 0) === 0);
+  var anyTwoSidesLessThanThird = (sides[0] + sides[1] < sides[2]
+    || sides[0] + sides[2] < sides[1]
+    || sides[1] + sides[2] < sides[0] );
+
+  if (hasNoSize) {return true};
+  if (anyTwoSidesLessThanThird) {return true};
+
+  return false;
+};
+
+var numberOfEqualSides = sides => {
+  var arrOfEqualSides = sides.map(oneSide => {
+    return sides.filter(side => side === oneSide).length;
   });
   var numberOfEqualSides = Math.max(...arrOfEqualSides)
   return numberOfEqualSides;
 }
 
-Triangle.prototype.kind = function () {
-  if (invalid(this.sides)) {
-    throw new Error();
-  }
-
+Triangle.prototype.kind = function() {
+  if (invalid(this.sides)) { throw new Error(); };
   return names[numberOfEqualSides(this.sides)];
-
 };
 
 module.exports = Triangle;
