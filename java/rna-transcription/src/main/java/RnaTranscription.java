4b4e21c4d1c6630f8af3b68dee2bfc371c8f18af
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-03-14 21:09:09 +0000

    works for signe rna

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class RnaTranscription {
     public String ofDna(String dnaString) {
-        throw new UnsupportedOperationException("Method has not been implemented yet.");
+      String result = "";
+      if (!(dnaString.equals(""))) {
+        HashMap<String, String> complements = new HashMap<>();
+        complements.put("G", "C");
+        complements.put("C", "G");
+        complements.put("T", "T");
+        complements.put("A", "U");
+        result = complements.get(dnaString);
+      }
+
+
+      return result;
     }
 }
