ec0405e73ab31006dd230de152e1b1c6ac57a2db
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-03-17 11:18:46 +0000

    tests passing

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,6 +1,21 @@
 public class PangramChecker {
 
-    public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Method has not been implemented yet.");
+  public boolean isPangram(String input) {
+    boolean result = true;
+    input = input.toLowerCase();
+    String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+    if (input.equals("")) {
+      result = false;
+      return result;
+    } else {
+      for (String letter : alphabet) {
+        if (!(input.contains(letter))) {
+          result = false;
+          return result;
+        }
+      }
     }
+    return result;
+  }
 }
